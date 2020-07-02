7e9a69b3f24a9987f6f4aec4b0520ccb9c19e283
Paul Chin Jr <pchinjr@gmail.com>
2020-07-02 12:07:55 -0400

    public method

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,3 +1,4 @@
 let arc = require('@architect/functions')
 
-exports.handler = arc.http.proxy( { }, console.log('Praise Cage'))
+
+exports.handler = arc.http.proxy.public( { }, console.log('Praise Cage'))
